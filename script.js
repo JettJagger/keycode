31de4ffb8d0dd629f419fd0ec91547275cc11fb4
Jett Jagger <jettrjagger@hotmail.com>
2023-08-18 11:54:31 +0100

    key3

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let keyCode = document.getElementById('keyCode');
     let charCode = document.getElementById('charCode');
 
+// brings up key/character code/value when key is pressed 
     document.addEventListener('keypress', (event) => {
         keyValue.textContent = "Key Value: " + event.key;
         keyCode.textContent = "Key Code: " + event.code;
